3f30522de5bc82b3cc167c40e86a862c7345985b
Assin Anna <assinannameckal@gmail.com>
2025-06-16 18:14:05 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { Home } from './Pages/Home';
 
+
 function App() {
   return ( 
     <div className="App">
       <Home />
+     
     </div>
   );
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
-import classDatajson from '../MyData/data.json';
+import classDataJson from '../MyData/data.json';
 import "./Home.css";
 
 type Subject = {
-    subject : string,
-    mark : number
-}
+  subject: string;
+  mark: number;
+};
 
 type Student = {
-    name : string,
-    id : number,
-    marks : Subject[]
-}
+  name: string;
+  id: string;
+  marks: Subject[];
+};
 
 type ClassData = {
-    teacherName : string,
-    name : string,
-    students : Student[]
-}
+  name: string;
+  teachersName: string;
+  students: Student[];
+};
 
-export const Home = () => {
+const data : ClassData = classDataJson as ClassData;
+
+export function Home(){
   return (
     <div>
-      <h1 className='heading'>School List Panel</h1>
+      <h1 className='heading'>Students Panel List</h1>
+      <div className='main-container'>
+        <h1 className='class'>CLASS:{data.name}</h1>
+        <h2 className='teacher'>TEACHER: {data.teachersName}</h2>
+        <h3 className='students'>Students:
+      <ul className='stud-data'>
+         <li className='details'>
+            
+         </li>
+      </ul>
+    </h3>
+  </div> 
     </div>
+    
   );
 };
 
